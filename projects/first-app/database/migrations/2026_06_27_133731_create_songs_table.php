9401e355759cccbb09d4c6a9c7b71636df9f6454
Maanik Arya <maanikarya200@gmail.com>
2026-06-27 19:51:58 +0530

    Creating a Sporify clone in the first-app folder in the laravel projects.

---
+++ projects/first-app/database/migrations/2026_06_27_133731_create_songs_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('songs', function (Blueprint $table) {
+            $table->id();
+            $table->foreign('album_id')->references('id')->on('albums');
+            $table->foreign('artist_id')->references('id')->on('artists');
+            $table->foreign('genre_id')->references('id')->on('genres');
+            $table->string('title');
+            $table->string('duration');
+            $table->string('audio_path');
+            $table->string('cover_image');
+            $table->integer('play_count');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('songs');
+    }
+};
